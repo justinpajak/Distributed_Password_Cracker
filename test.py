53b499cfc0d4bb52c5eb0976f5941c59e14944bc
errmeier <62953732+errmeier@users.noreply.github.com>
2021-11-13 13:36:53 -0500

    add comments to test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
+# How I was thinking iterating through the possible permutations should work - replace chr() with our int_to_char()
 n = 26
-seq = [0, 0]
-while seq != [3, 0, 0]:
+seq = [0, 0] # start sequence
+while seq != [3, 0, 0]: # end sequence
     print(seq, ''.join(chr(c + 65) for c in seq))
     i = len(seq) - 1
     seq[-1] += 1
@@ -12,7 +13,7 @@ while seq != [3, 0, 0]:
             i -= 1
             seq[i] += 1
 
-
+# Decoding all possible values of i for passwords of length 3
 n = 26
 length = 3
 for i in range(0, n**length):
