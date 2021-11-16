e45c21459941b9bd52173bc3489a6f333511d057
erik <emeier@nd.edu>
2021-11-16 15:55:16 -0500

    manager updates

--- manager.py
+++ manager.py
@@ -28,6 +28,7 @@ class Manager:
     def __init__(self, max_length, batch_size):
         self.workers = {}
         self.hashes = []
+        self.cracked = []
         self.batch_size = batch_size
         self.available = [[length, 0, SYMBOLS**length - 1] for length in range(1,
             max_length+1)]
@@ -39,7 +40,7 @@ class Manager:
 
     def batch(self):
         if not self.available:
-            return None
+            return None, None
         
         to_batch = self.batch_size
         start = [self.available[0][0], self.available[0][1]]
@@ -47,7 +48,7 @@ class Manager:
         while to_batch > 0:
             if to_batch >= self.available[0][2] - self.available[0][1]:
                 if len(self.available) == 1:
-                    end[0], end[1] = self.available[0], self.available[2]
+                    end[0], end[1] = self.available[0][0], self.available[0][2]
                     self.available.pop(0)
                     break
                 else:
@@ -67,13 +68,56 @@ class Manager:
         self.workers[conn.fileno()] = {"conn": conn, "lastheardfrom": time.time()}
         start, end = self.batch()
         self.workers[conn.fileno()]["interval"] = (start, end)
-        order = {"cracked": {}, "start": [start[0], start[1]], "end": [end[0], end[1]]}
-        for h in self.hashes:
-            order["cracked"][h] = False
+        order = {"crack": [h for h in self.hashes], "start": [start[0], start[1]], "end": [end[0], end[1]]}
         
-        conn.sendall(json.dumps(order).encode('utf-8'))
-        print("Sent work order", json.dumps(order).encode('utf-8'))
+        message = json.dumps(order)
+        message = len(message).to_bytes(8, "little") + message.encode("ascii")
+        conn.sendall(message)
+        print(f"Sent interval to {conn.fileno}: {start} {end}")
 
+    def update_worker(self, conn):
+        # Receive message from worker
+        message_len = conn.recv(8)
+        message_len = int.from_bytes(message_len, "little")
+
+        message = ""
+        while len(message) < message_len:
+            message += conn.recv(512).decode("ascii")
+
+        message_json = json.loads(message)
+        if message_json['status'] == 'failure':
+            raise ConnectionResetError
+        cracked = message_json['cracked']
+        for c in cracked:
+            self.hashes.remove(c)
+            self.cracked.append((cracked[c], c))
+
+        # Update manager-side info
+        self.workers[conn.fileno()]["lastheardfrom"] = time.time()
+        start, end = self.batch()
+        self.workers[conn.fileno()]["interval"] = (start, end)
+        
+        # Update worker with new batch
+        order = {"crack": [h for h in self.hashes], "start": [start[0], start[1]], "end": [end[0], end[1]]}
+
+        conn.sendall(json.dumps(order).encode('utf-8'))
+        print(f"Sent interval to {conn.fileno()}: {start} {end}")
+
+    def cleanup(self, conn):
+        w = self.workers.pop(conn.fileno())
+        if w["interval"] not in self.working:
+            return
+        self.working.remove(w["interval"])
+        for length in range(w["interval"][0][0], w["interval"][1][0]+1):
+            if length == w["interval"][0][0]:
+                self.available.insert(0, [length, w["interval"][0][1], SYMBOLS**length - 1])
+            elif length == w["interval"][1][0]:
+                self.available.insert(0, [length, 0, w["interval"][1][1]])
+            else:
+                self.available.insert(0, [length, 0, SYMBOLS**length - 1])
+
+        print("Available Intervals: ", self.available)
+         
 
 def usage():
     print(f"Usage: {sys.argv[0]} <hashfile>")
@@ -107,6 +151,8 @@ def handle_input(m, command):
         print(command[1])
     elif command[0] == "system":
         print(m.workers)
+    else:
+        print("Invalid Command")
     print("> ", end="", flush=True)
 
 if __name__ == "__main__":
@@ -114,23 +160,27 @@ if __name__ == "__main__":
         usage()
     m = Manager(3, 9999)
     m.load_hashes(sys.argv[1])
-    hostname = socket.gethostname()
+    hostname = socket.gethostbyname(socket.gethostname())
     projname = "dps-manager"
 
+    # Open up server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((hostname, 0))
     port = server.getsockname()[1]
     socks = [server, sys.stdin]
-    print(f'Manager listening on port {port}')
     server.listen()
     
+    # Start thread to constantly update name server
     ns_thread = threading.Thread(target=update_ns, args=(projname, port))
     ns_thread.start()
     
+    # Set up interactive shell
     os.system('clear')
-    print("Distributed Password Cracker - Manager")
+    print(f"Distributed Password Cracker - Manager (Port {port})")
     print("-- type help for command list")
     print("\n> ", end="", flush=True)
+    
+    # Main loop
     while True:
         r, w, x = select.select(socks, [], [])
         for s in r:
@@ -140,9 +190,16 @@ if __name__ == "__main__":
                 socks.append(conn)
                 m.accept_worker(conn)
             elif s == sys.stdin:
-                handle_input(m, sys.stdin.readline().strip())
+                try:
+                    handle_input(m, sys.stdin.readline().strip())
+                except:
+                    print("Invalid Command")
+                    print("> ", end="", flush=True)
             else:
-                pass
-                    #m.cleanup(s)
+                try:
+                    m.update_worker(s)
+                except:
+                    m.cleanup(s)
+                    socks.remove(s)
                 
 
